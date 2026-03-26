71c72a80d1d00fdf02e535ffa99fa63165a0c00b
Bruno Benjamim <brtj123@gmail.com>
2026-03-26 14:26:21 -0300

    Add SQL query to count films by year

--- Exercício_Filmes.sql
+++ Exercício_Filmes.sql
@@ -1,1 +1,17 @@
+-- Exercício 07
+SELECT 
+    Ano,
+    COUNT(*) AS QuantidadeFilmes
+FROM Filmes
+GROUP BY Ano
+ORDER BY QuantidadeFilmes DESC;
 
+-- Exercício 08
+
+-- Exercício 09
+
+-- Exercício 10
+
+-- Exercício 11
+
+-- Exercício 12
