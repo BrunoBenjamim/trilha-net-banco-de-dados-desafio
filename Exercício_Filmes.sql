c1498e6f1fc9e350172c79a4a6aa6ec98f5a82ea
Bruno Benjamim <brtj123@gmail.com>
2026-03-26 15:13:05 -0300

    Add SQL queries for film exercises

--- Exercício_Filmes.sql
+++ Exercício_Filmes.sql
@@ -1,4 +1,47 @@
--- Exercício 07
+-- Exercício 01 --
+SELECT
+    Nome,
+    Ano
+FROM Filmes;
+
+-- Exercício 02 --
+SELECT
+    Nome,
+    Ano,
+    Duracao
+FROM Filmes
+ORDER BY Ano;
+    
+-- Exercício 03 --
+SELECT
+    Nome,
+    Ano,
+    Duracao
+WHERE Nome = 'De Volta para o Futuro';
+
+-- Exercício 04 --
+SELECT
+    Nome,
+    Ano,
+    Duracao
+WHERE Ano = 1997;
+
+-- Exercício 05 --
+SELECT
+    Nome,
+    Ano,
+    Duracao
+WHERE Ano > 2000;
+
+-- Exercício 06 --
+SELECT
+    Nome,
+    Ano,
+    Duracao
+ORDER BY Duracao
+WHERE Duracao > 100 AND Duracao < 150;
+
+-- Exercício 07 --
 SELECT 
     Ano,
     COUNT(*) AS QuantidadeFilmes
@@ -6,12 +49,46 @@ FROM Filmes
 GROUP BY Ano
 ORDER BY QuantidadeFilmes DESC;
 
--- Exercício 08
+-- Exercício 08 --
+SELECT
+    PrimeiroNome,
+    UltimoNome,
+    Genero
+FROM Atores
+WHERE Genero = 'M';
 
--- Exercício 09
+-- Exercício 09 --
+SELECT
+    PrimeiroNome,
+    UltimoNome,
+    Genero
+FROM Atores
+ORDER BY PrimeiroNome
+WHERE Genero = 'F';
 
--- Exercício 10
+-- Exercício 10 --
+SELECT 
+    f.Nome AS Filme,
+    g.Genero
+FROM Filmes f
+INNER JOIN FilmesGenero fg ON fg.IdFilme = f.Id
+INNER JOIN Generos g ON g.Id = fg.IdGenero;
 
--- Exercício 11
+-- Exercício 11 --
+SELECT 
+    f.Nome AS Filme,
+    g.Genero
+FROM Filmes f
+INNER JOIN FilmesGenero fg ON fg.IdFilme = f.Id
+INNER JOIN Generos g ON g.Id = fg.IdGenero
+WHERE g.Genero = 'Mistério';
 
--- Exercício 12
+-- Exercício 12 --
+SELECT 
+    f.Nome AS Filme,
+    a.PrimeiroNome,
+    a.UltimoNome,
+    ef.Papel
+FROM Filmes f
+INNER JOIN ElencoFilme ef ON ef.IdFilme = f.Id
+INNER JOIN Atores a ON a.Id = ef.IdAtor;
